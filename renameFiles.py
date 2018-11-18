b61d795bc3da4d6191380fba0e4a6ba98c7c1273
Bush <dbush@alionscience.com>
2018-11-17 20:13:03 -0500

    enhancement for adding filenames to error input

--- renameFiles.py
+++ renameFiles.py
@@ -70,13 +70,14 @@ for spID in range(1,850):
     if nSpineNums == 0:
         missingSpines.append(spID)
 totMissed = len(missingSpines)+len(badSpines)
-print(f'we are missing a total of {totMissed} spine numbers between 1 and 850 (a total of {850-totMissed})')
 
 # now lets get a file list of all of the files in the directory
 fileNames = [f for f in listdir(inputPath) if isfile(join(inputPath, f))]
 
 #open the error file for writing
 fp = open(errorFile,'w') 
+fp.write(f'Read {line_count} rows from the the excel doc, found {entry_count} valid bluerays.\n')
+fp.write(f'we are missing a total of {totMissed} spine numbers between 1 and 850 (a total of {850-totMissed}) in the excel file\n')
 
 nFilesMissed = 0
 nFiles = 0
@@ -91,15 +92,15 @@ for f in fileNames:
                         if int(sn2) not in spine:
                             if int(sn2) in missingSpines:
                                 #print(f'found cover for {sn2}, but we are missing it from the file')
-                                fp.write(f'found cover for {sn2}, but we are missing it from the file\n') 
+                                fp.write(f'found cover for {sn2} ({f}), but we are missing it from the file\n') 
                                 nFilesMissed += 1
                             else:
-                                fp.write(f'found cover for {sn2}, but we are missing it from the file (error case)\n') 
+                                fp.write(f'found cover for {sn2} ({f}), but we are missing it from the file (error case)\n') 
                                 print(f'-------ERROR:spine: {sn2} not found--------')
                                 nFilesMissed += 1
                         elif int(sn2) in badSpines:
                             #print(f'skipping {sn2} because there are multiples')
-                            fp.write(f'skipping {sn2} because there are multiples\n') 
+                            fp.write(f'skipping {sn2} ({f}) because there are multiples\n') 
                             xx=2
                         else:
                             spIdx = spine.index(int(sn2))
@@ -110,10 +111,11 @@ for f in fileNames:
                             nFiles += 1
                 else:
                     #print(f'spine: {sn2} bad')
-                    fp.write(f'skip {sn2}, because I assume it is not an actual cover\n') 
+                    fp.write(f'skip {sn2} ({f}), because I assume it is not an actual cover\n') 
                     nFilesBad += 1
 
 
 print(f'there are {nFiles} files to be renamed, and we missed {nFilesMissed}, with {nFilesBad} skipped')
+fp.write(f'there were {nFiles} files renamed, and we missed {nFilesMissed}, with {nFilesBad} skipped (all noted above)\n')
 fp.close() 
 
